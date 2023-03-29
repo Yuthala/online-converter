33dbd6e6d29ca0e7b389db4a64d349f83e0d1421
Dina Polousova <dinapolousova@gmail.com>
2023-03-29 19:32:05 +0300

    header correction (CORS)

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ let inputRub = document.getElementById('rub'),
 
 inputRub.addEventListener('input', () => {
     let request = new XMLHttpRequest(); //создаем новый объект для работы с сетевыми запросами
-    console.log(`${request} - переменная request`);
     request.open('GET', 'https://www.cbr-xml-daily.ru/daily_json.js');
 
-    request.setRequestHeader('Content-type', 'application/json:charset=utf-8');
+    request.setRequestHeader('Content-type', 'text/plain');
 
     request.send(); //тело запроса
 
-    request.addEventListener('load', function() {
+    request.addEventListener('readystatechange', function() {
         if(request.readyState === 4 && request.status == 200) { //статус нашего запроса = 4 (DONE) и статус сервера 200 (OPEN)
             let data = JSON.parse(request.response).Valute.USD.Value.toFixed(2); //сохраняем в переменную data данные, полученные от сервера
-            console.log(`${data} - переменная data`);
 
             inputUsd.value = inputRub.value /  data;
         } else {
